3ae2f6d922e73da46688d34f2234c74235470240
Mehmet Kanat <mehmet.kanat@protonmail.com>
2020-11-26 22:14:02 +0300

    Add middleware

---
+++ src/middleware/Authentication.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\middleware;
+
+use App\CustomException;
+use \Psr\Http\Message\ServerRequestInterface;
+use \Psr\Http\Message\ResponseInterface;
+
+class Authentication extends Middleware
+{
+    public function __invoke(ServerRequestInterface $request, ResponseInterface $response, $next)
+    {
+
+        if (!isset($_SESSION['userInfos']) || !$_SESSION['userInfos']['user_id']) {
+
+            if (!empty($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest') {
+                throw CustomException::clientError(401, 'Please log in again!');
+            }
+
+            return $response->withRedirect($this->container->router->pathFor('login'));
+        }
+
+        $response = $next($request, $response);
+
+        return $response;
+
+    }
+}
