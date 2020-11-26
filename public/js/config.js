6917c908124382c91c533bb557f432d6d2bab9e4
Mehmet Kanat <mehmet.kanat@protonmail.com>
2020-11-26 21:46:57 +0300

    Add css, js, font files

---
+++ public/js/config.js
@@ -0,0 +1,32 @@
+"use strict";
+var base = {
+        defaultFontFamily: "Overpass, sans-serif",
+        primaryColor: "#1b68ff",
+        secondaryColor: "#4f4f4f",
+        successColor: "#3ad29f",
+        warningColor: "#ffc107",
+        infoColor: "#17a2b8",
+        dangerColor: "#dc3545",
+        darkColor: "#343a40",
+        lightColor: "#f2f3f6"
+    }, extend = {
+        primaryColorLight: tinycolor(base.primaryColor).lighten(10).toString(),
+        primaryColorLighter: tinycolor(base.primaryColor).lighten(30).toString(),
+        primaryColorDark: tinycolor(base.primaryColor).darken(10).toString(),
+        primaryColorDarker: tinycolor(base.primaryColor).darken(30).toString()
+    }, chartColors = [base.primaryColor, base.successColor, "#6f42c1", extend.primaryColorLighter], colors = {
+        bodyColor: "#6c757d",
+        headingColor: "#495057",
+        borderColor: "#e9ecef",
+        backgroundColor: "#f8f9fa",
+        mutedColor: "#adb5bd",
+        chartTheme: "light"
+    }, darkColor = {
+        bodyColor: "#adb5bd",
+        headingColor: "#e9ecef",
+        borderColor: "#212529",
+        backgroundColor: "#495057",
+        mutedColor: "#adb5bd",
+        chartTheme: "dark"
+    }, curentTheme = localStorage.getItem("mode"), dark = document.querySelector("#darkTheme"),
+    light = document.querySelector("#lightTheme"), switcher = document.querySelector("#modeSwitcher");
