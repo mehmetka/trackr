99568d71edc52e7ebdea8a52baf74838e3831760
Mehmet Kanat <mehmet.kanat@protonmail.com>
2020-11-26 21:57:35 +0300

    Add base controller

---
+++ src/controller/Controller.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\controller;
+
+use Psr\Container\ContainerInterface;
+use Slim\Http\Response;
+
+
+class Controller
+{
+    protected $container;
+    protected $view;
+
+    public function __construct(ContainerInterface $container)
+    {
+        $this->container = $container;
+        $this->view = $container->get('view');
+    }
+
+    public function __get($property)
+    {
+        if ($this->container->{$property}) {
+            return $this->container->{$property};
+        }
+    }
+
+    public function response($status = 200, $data = [], $allow = [])
+    {
+        if (!isset($this->response)) {
+            $this->response = new  Response($status);
+        }
+
+        /** @var \Psr\Http\Message\ResponseInterface $response */
+        $response = $this->response->withStatus($status);
+
+        if (!empty($allow)) {
+            $response = $response->withHeader('Allow', strtoupper(implode(',', $allow)));
+        }
+
+        if (!empty($data)) {
+            $response = $response->withJson($data);
+        }
+
+        return $response;
+    }
+
+}
