e302bb8ee87243cfc7fd0e2f1f75a71c8315c38f
Mehmet Kanat <mehmet.kanat@protonmail.com>
2020-11-26 22:39:06 +0300

    Refactoring

--- src/routes.php
+++ src/routes.php
@@ -1,14 +1,16 @@
 <?php
 
 use App\middleware;
+use App\controller\AuthController;
+use App\controller\HomeController;
 
 $app->group('', function () {
-    $this->get('/login', \App\controller\AuthController::class . ':index')->setName('login');
-    $this->post('/login', \App\controller\AuthController::class . ':login');
+    $this->get('/login', AuthController::class . ':index')->setName('login');
+    $this->post('/login',AuthController::class . ':login');
 });
 
 $app->group('', function () {
 
-    $this->get('/', \App\controller\HomeController::class . ':index')->setName('home');
+    $this->get('/',HomeController::class . ':index')->setName('home');
 
 })->add(new Middleware\Authentication($container));
