09c3a98ea12466016929c6f1d21b0534dcaa9505
Mehmet Kanat <mehmet.kanat@protonmail.com>
2020-11-27 22:42:04 +0300

    Add util class for general purpose

---
+++ src/util/Util.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\util;
+
+class Util
+{
+    static function dateDiff($d1, $d2)
+    {
+        return round(abs(strtotime($d1) - strtotime($d2)) / 86400);
+    }
+
+    static function getDayDifference($from, $to)
+    {
+        $interval = date_diff(date_create($from), date_create($to));
+        return intval($interval->format('%a')) + 1;
+    }
+
+
+    static function epochDateDiff($d1, $d2)
+    {
+        return round(abs($d1 - $d2) / 86400);
+    }
+
+    static function calculateAge($d1, $d2)
+    {
+        return round(round(abs(strtotime($d1) - strtotime($d2)) / 86400) / 365, 3);
+    }
+}
