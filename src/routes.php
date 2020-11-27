1e4cf27cad6dc2f2c7e2c6210c323e4bd624277b
Mehmet Kanat <mehmet.kanat@protonmail.com>
2020-11-27 23:41:33 +0300

    Add books path inside route

--- src/routes.php
+++ src/routes.php
@@ -15,6 +15,7 @@ $app->group('', function () {
     $this->get('/', HomeController::class . ':index')->setName('home');
 
     $this->get('/books/paths', BookController::class . ':paths')->setName('paths');
+    $this->get('/books/paths/{pathId:[0-9]+}', BookController::class . ':booksPathInside');
     $this->get('/all-books', BookController::class . ':allBooks');
     $this->get('/my-books', BookController::class . ':myBooks');
     $this->get('/books/finished', BookController::class . ':finishedBooks');
