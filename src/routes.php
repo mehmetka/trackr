0e14f50aeacf090c19b2c67d69091568138a6eeb
Mehmet Kanat <mehmet.kanat@protonmail.com>
2020-11-27 22:43:38 +0300

    Reformat, edit class path usage and add books paths page

--- src/routes.php
+++ src/routes.php
@@ -3,16 +3,19 @@
 use App\middleware;
 use App\controller\AuthController;
 use App\controller\HomeController;
+use App\controller\BookController;
 
 $app->group('', function () {
     $this->get('/login', AuthController::class . ':index')->setName('login');
-    $this->post('/login',AuthController::class . ':login');
+    $this->post('/login', AuthController::class . ':login');
 });
 
 $app->group('', function () {
 
-    $this->get('/',HomeController::class . ':index')->setName('home');
+    $this->get('/', HomeController::class . ':index')->setName('home');
 
-    $this->get('/logout', \App\controller\AuthController::class . ':logout');
+    $this->get('/books/paths', BookController::class . ':paths')->setName('paths');
+
+    $this->get('/logout', AuthController::class . ':logout');
 
 })->add(new Middleware\Authentication($container));
