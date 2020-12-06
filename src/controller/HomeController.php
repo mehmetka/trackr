2736fae216766ba587b2efe82185009f52262e47
Mehmet Kanat <mehmet.kanat@protonmail.com>
2020-12-06 16:43:24 +0300

    Remove redirect from home and add reading average

--- src/controller/HomeController.php
+++ src/controller/HomeController.php
@@ -2,20 +2,35 @@
 
 namespace App\controller;
 
+use App\model\BookModel;
 use \Psr\Http\Message\ServerRequestInterface;
 use \Psr\Http\Message\ResponseInterface;
 use Psr\Container\ContainerInterface;
 
 class HomeController extends Controller
 {
+    private $bookModel;
+
     public function __construct(ContainerInterface $container)
     {
         parent::__construct($container);
+        $this->bookModel = new BookModel($container);
     }
 
     public function index(ServerRequestInterface $request, ResponseInterface $response)
     {
-        return $response->withRedirect($this->container->router->pathFor('paths'), 302);
+        $averageData = $this->bookModel->readingAverage();
+        $today = date('m/d/Y');
+
+        $data = [
+            'readingAverage' => round($averageData['average'], 3),
+            'readingTotal' => $averageData['total'],
+            'dayDiff' => $averageData['diff'],
+            'today' => $today,
+            'activeHome' => 'active'
+        ];
+
+        return $this->view->render($response, 'home.mustache', $data);
     }
 
 }
