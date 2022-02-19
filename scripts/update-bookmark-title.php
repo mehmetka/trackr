c6a089633961cfc5b66018497196dc4576dd405f
Mehmet Kanat <mehmet.kanat@protonmail.com>
2022-02-20 01:52:59 +0300

    Add bookmark title async updater script

---
+++ scripts/update-bookmark-title.php
@@ -0,0 +1,95 @@
+<?php
+
+$conf = parse_ini_file(__DIR__ . '/../conf.ini', true);
+
+try {
+    $dbConnection = new \PDO("mysql:host={$conf['db']['host']}:3306;dbname={$conf['db']['database']};charset=utf8mb4", $conf['db']['user'], $conf['db']['password']);
+} catch (Exception $e) {
+    echo 'Database access problem: ' . $e->getMessage();
+    die;
+}
+
+if(isset($argv[1])){
+    $bookmarkDetails = getBookmarkByID($argv[1]);
+    $title = getTitle($bookmarkDetails['bookmark']);
+    $title =  trim(strip_tags($title));
+
+    if($title){
+        updateBookmarkTitle($bookmarkDetails['id'], $title);
+    }
+
+} 
+
+die;
+
+function getBookmarkByID($bookmarkID)
+{
+    $dbConnection = $GLOBALS['dbConnection'];
+
+    $sql = 'SELECT * FROM bookmarks WHERE id = :id';
+
+    $stm = $dbConnection->prepare($sql);
+    $stm->bindParam(':id', $bookmarkID, \PDO::PARAM_INT);
+
+    if (!$stm->execute()) {
+        return false;
+    }
+
+    $bookmark = [];
+
+    while ($row = $stm->fetch(\PDO::FETCH_ASSOC)) {
+        print_r($row);
+        $bookmark = $row;
+    }
+
+    return $bookmark;
+}
+
+function getTitle($url)
+{
+    try {
+        $data = @file_get_contents($url);
+        $code = getHttpCode($http_response_header);
+
+        if ($code === 404) {
+            return '404 Not Found';
+        }
+    } catch (\Exception $exception) {
+        return null;
+    }
+
+    if (preg_match('/<title[^>]*>(.*?)<\/title>/ims', $data, $matches)) {
+        return mb_check_encoding($matches[1], 'UTF-8') ? $matches[1] : utf8_encode($matches[1]);
+    }
+
+    return null;
+}
+
+function getHttpCode($http_response_header)
+{
+    if (is_array($http_response_header)) {
+        $parts = explode(' ', $http_response_header[0]);
+        if (count($parts) > 1) //HTTP/1.0 <code> <text>
+            return intval($parts[1]); //Get code
+    }
+    return 0;
+}
+
+function updateBookmarkTitle($bookmarkID, $title)
+{
+    $dbConnection = $GLOBALS['dbConnection'];
+
+    $sql = 'UPDATE bookmarks SET title = :title WHERE id = :id';
+
+    $stm = $dbConnection->prepare($sql);
+    $stm->bindParam(':id', $bookmarkID, \PDO::PARAM_INT);
+    $stm->bindParam(':title', $title, \PDO::PARAM_STR);
+
+    if (!$stm->execute()) {
+        echo "fail\n";
+        return false;
+    }
+
+    echo "success\n";
+    return true;
+}
