b2a48f6d39b056df8f9d0f907a8e4f178835a294
Mehmet Kanat <mehmet.kanat@protonmail.com>
2022-04-17 01:01:29 +0300

    Add bookmark title fetcher background script

--- scripts/update-bookmark-title.php
+++ scripts/update-bookmark-title.php
@@ -1,21 +1,11 @@
 <?php
 
-require __DIR__ . '/../vendor/autoload.php';
-
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
-$dotenv->load();
-
-try {
-    $dbConnection = new \PDO("mysql:host={$_ENV['MYSQL_HOST']}:3306;dbname={$_ENV['MYSQL_DATABASE']};charset=utf8mb4", $_ENV['MYSQL_USER'], $_ENV['MYSQL_PASSWORD']);
-} catch (Exception $e) {
-    echo 'Database access problem: ' . $e->getMessage();
-    die;
-}
+include_once __DIR__ . '/init.php';
 
 if(isset($argv[1])){
     $bookmarkDetails = getBookmarkByID($argv[1]);
     $title = getTitle($bookmarkDetails['bookmark']);
-    $title =  trim(strip_tags($title));
+    $title =  strip_tags(trim($title));
 
     if($title){
         updateBookmarkTitle($bookmarkDetails['id'], $title);
