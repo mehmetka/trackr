6db5745ccc285bff56a6923bfed8577f2e1caa78
Mehmet Kanat <mehmet.kanat@protonmail.com>
2022-08-21 00:09:53 +0300

    Add multiple user support for date tracking

--- src/model/DateTrackingModel.php
+++ src/model/DateTrackingModel.php
@@ -41,9 +41,11 @@ class DateTrackingModel
         $list = [];
 
         $sql = 'SELECT id, name, start, created
-                FROM date_trackings';
+                FROM date_trackings
+                WHERE user_id = :user_id';
 
         $stm = $this->dbConnection->prepare($sql);
+        $stm->bindParam(':user_id', $_SESSION['userInfos']['user_id'], \PDO::PARAM_INT);
 
         if (!$stm->execute()) {
             throw CustomException::dbError(503, json_encode($stm->errorInfo()));
