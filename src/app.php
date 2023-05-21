5002f00cc0246790c204a70b301eddef245c00fc
Mehmet Kanat <mehmet.kanat@protonmail.com>
2023-05-21 14:13:46 +0300

    Change session lifetime

--- src/app.php
+++ src/app.php
@@ -8,13 +8,14 @@ error_reporting(E_ERROR);
 date_default_timezone_set('Europe/Istanbul');
 
 session_name('trackr');
+ini_set('session.gc_maxlifetime', 7200); // 7200 seconds - 120 minutes
 ini_set('session.cookie_httponly', 1);
 session_start();
 
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
 $dotenv->load();
 
-if(!strpos($_ENV['TRACKR_BASE_URL'], $_SERVER['HTTP_HOST'])){
+if (!strpos($_ENV['TRACKR_BASE_URL'], $_SERVER['HTTP_HOST'])) {
     header('location: ' . $_ENV['TRACKR_BASE_URL']);
     exit();
 }
