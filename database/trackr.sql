64b1e0e4878aa4f340e6ff98d551a62047c92e1e
Mehmet Kanat <mehmet.kanat@protonmail.com>
2023-06-09 18:41:09 +0300

    Add images table

--- database/trackr.sql
+++ database/trackr.sql
@@ -203,6 +203,18 @@ CREATE TABLE `highlight_versions`
   DEFAULT CHARSET = utf8mb4
   COLLATE = utf8mb4_unicode_ci;
 
+CREATE TABLE `images`
+(
+    `id`         int(11)                                NOT NULL AUTO_INCREMENT,
+    `sha1`       varchar(45) COLLATE utf8mb4_unicode_ci NOT NULL,
+    `filename`   varchar(45) COLLATE utf8mb4_unicode_ci NOT NULL,
+    `created_at` int(11)                                NOT NULL,
+    `user_id`    int(11)                                NOT NULL,
+    PRIMARY KEY (`id`)
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4
+  COLLATE = utf8mb4_unicode_ci;
+
 CREATE TABLE `logs`
 (
     `id`      int(11)                                NOT NULL AUTO_INCREMENT,
