a53dffdec028a462583fb142848af0cf382a846d
Mehmet Kanat <mehmet.kanat@protonmail.com>
2023-12-11 10:15:08 +0300

    Fix insert problem

--- src/model/DateTrackingModel.php
+++ src/model/DateTrackingModel.php
@@ -21,13 +21,14 @@ class DateTrackingModel
     {
         $created = time();
 
-        $sql = 'INSERT INTO date_trackings (name, start, created)
-                VALUES(:name, :start, :created)';
+        $sql = 'INSERT INTO date_trackings (name, start, created, user_id)
+                VALUES(:name, :start, :created, :user_id)';
 
         $stm = $this->dbConnection->prepare($sql);
         $stm->bindParam(':name', $name, \PDO::PARAM_STR);
         $stm->bindParam(':start', $start, \PDO::PARAM_STR);
         $stm->bindParam(':created', $created, \PDO::PARAM_INT);
+        $stm->bindParam(':user_id', $_SESSION['userInfos']['user_id'], \PDO::PARAM_INT);
 
         if (!$stm->execute()) {
             throw CustomException::dbError(StatusCode::HTTP_SERVICE_UNAVAILABLE, json_encode($stm->errorInfo()));
