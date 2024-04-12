d632a37c02526d8dda2d37dd7bf4c9f310133a17
Mehmet Kanat <mehmet.kanat@protonmail.com>
2024-04-12 15:07:42 +0300

    Fix navbar toggle problem

--- public/js/navbar.js
+++ public/js/navbar.js
@@ -1,12 +1,12 @@
 $(document).ready(function () {
     $(".collapseSidebar").on("click", function (e) {
-        if ($(".vertical").hasClass("narrow")) {
-            $(".vertical").toggleClass("open")
-            $('#highlight-favorites').toggle(); // TODO doesnt work
+        if ($(".vertical").hasClass("collapsed")) {
+            $('#highlight-favorites').toggle();
         } else {
             $('#highlight-favorites').hide();
-            ($(".vertical").toggleClass("collapsed"), $(".vertical").hasClass("hover") && $(".vertical").removeClass("hover")), e.preventDefault()
         }
+
+        ($(".vertical").toggleClass("collapsed"), $(".vertical").hasClass("hover") && $(".vertical").removeClass("hover")), e.preventDefault()
     });
 
     var resultNavbarInfos = ajaxCall('GET', "/navbar-infos", null, false, false);
