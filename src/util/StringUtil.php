7e71cde4d5472c884b85aa08712dd20b1148f427
Mehmet Kanat <mehmet.kanat@protonmail.com>
2024-05-05 23:17:19 +0300

    Make unique result array to prevent duplicate operations

--- src/util/StringUtil.php
+++ src/util/StringUtil.php
@@ -7,12 +7,12 @@ class StringUtil
     public static function getDecimalHashtags($text) : array
     {
         preg_match_all('/\B#\K([0-9]+)\b/', $text, $matches);
-        return $matches[0];
+        return array_unique($matches[0]);
     }
 
     public static function getAlphaNumericHashtags($text) : array
     {
         preg_match_all('/\B[#]\K[a-zA-Z0-9]+/', $text, $matches);
-        return $matches[0];
+        return array_unique($matches[0]);
     }
 }
