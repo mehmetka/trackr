a52f8d125f65dfbf7275dffa78a44d87d52efd31
Mehmet Kanat <mehmet.kanat@protonmail.com>
2024-05-05 22:59:31 +0300

    Improve decimal hashtag regex to detect only numerical tags

--- src/util/StringUtil.php
+++ src/util/StringUtil.php
@@ -6,7 +6,7 @@ class StringUtil
 {
     public static function getDecimalHashtags($text) : array
     {
-        preg_match_all('/\B[#]\K\d+/', $text, $matches);
+        preg_match_all('/\B#\K([0-9]+)\b/', $text, $matches);
         return $matches[0];
     }
 }
