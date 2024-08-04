79a7f336fc17990b0424b7d4d80d3142afdd280c
Mehmet Kanat <mehmet.kanat@protonmail.com>
2024-08-04 12:09:32 +0300

    Add toArray function

--- src/enum/BookStatus.php
+++ src/enum/BookStatus.php
@@ -9,4 +9,15 @@ enum BookStatus: int
     case DONE = 2;
     case ABANDONED = 3;
     case PRIORITIZED = 4;
+
+    public static function toArray(): array
+    {
+        return [
+            self::NEW->value,
+            self::STARTED->value,
+            self::DONE->value,
+            self::ABANDONED->value,
+            self::PRIORITIZED->value,
+        ];
+    }
 }
--- src/enum/PathStatus.php
+++ src/enum/PathStatus.php
@@ -6,4 +6,12 @@ enum PathStatus: int
 {
     case ACTIVE = 0;
     case DONE = 1;
+
+    public static function toArray(): array
+    {
+        return [
+            self::ACTIVE->value,
+            self::DONE->value,
+        ];
+    }
 }
