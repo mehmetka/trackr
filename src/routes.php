fbf5aa42dffd0ccbb1033f1429f463cef3eeb604
Mehmet Kanat <mehmet.kanat@protonmail.com>
2024-08-26 22:48:02 +0300

    Add constant setting update route

--- src/routes.php
+++ src/routes.php
@@ -87,6 +87,7 @@ $app->group('', function () {
 
     $this->get('/chains', ChainController::class . ':index');
     $this->post('/chains', ChainController::class . ':start');
+    $this->put('/chains/{uid}/constant', ChainController::class . ':updateConstant');
     $this->post('/chains/{uid}/links', ChainController::class . ':addLink');
     $this->put('/chains/{uid}/showInLogs', ChainController::class . ':updateShowInLogs');
     $this->get('/chains/{uid}/graphic', ChainController::class . ':getChainGraphicData');
