52bfa7e89c57518fd72cae9f20ff6cfb526c8cd3
Mehmet Kanat <mehmet.kanat@protonmail.com>
2024-09-01 13:13:17 +0300

    Add lang/en class

---
+++ src/util/lang/En.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\util\lang;
+
+class En
+{
+    const HIGHLIGHT_ADDED_BEFORE = 'Highlight might added before!';
+    const HIGHLIGHT_CANNOT_BE_NULL = 'Highlight cannot be null!';
+    const HIGHLIGHT_MUST_BE_LONGER = 'Highlight must be longer than 1 word!';
+    const HIGHLIGHT_NOT_FOUND = 'Highlight not found!';
+    const HIGHLIGHT_SUCCESSFULLY_ADDED = 'Highlight successfully added!';
+    const HIGHLIGHT_SUB_SUCCESSFULLY_ADDED = 'Sub highlight successfully added!';
+    const HIGHLIGHT_SUCCESSFULLY_UPDATED = 'Highlight successfully updated!';
+    const HIGHLIGHT_NOT_EDITABLE = 'Highlight not editable!';
+    const HIGHLIGHT_PARENT_NOT_FOUND = 'Parent highlight not found!';
+    const HIGHLIGHT_DELETED_SUCCESSFULLY = 'Highlight deleted successfully!';
+
+    const SUCCESS = 'Success!';
+}
